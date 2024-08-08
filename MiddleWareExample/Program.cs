f426fdeb029c059694dcb11d4a7553dda432e534
Kavish-Evince <kavish@evincedev.com>
2024-08-08 14:47:12 +0530

    master:middleWare chain
    middleWare chain

--- MiddleWareExample/Program.cs
+++ MiddleWareExample/Program.cs
@@ -3,4 +3,23 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+//middleware 1 
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+   await context.Response.WriteAsync("Hello");
+    await next(context);
+});
+
+//middleware 2
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Hello again");
+    await next(context);
+});
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Hello twice");
+});
+
 app.Run();
